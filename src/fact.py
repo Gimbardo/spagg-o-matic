5d872a6b0a391b8967f2e57b55316c8a67281123
Gimbardo <gamberoatomico@gmail.com>
2022-08-05 17:47:40 +0200

    :art: removed facterinator and created fact class

---
+++ src/fact.py
@@ -0,0 +1,10 @@
+
+class Fact:
+  def __init__(self, content):
+    content = content.replace("–","-")
+    self.year = content.split("-")[0].strip()
+    self.info = '-'.join(content.split("-")[1:])[1:]
+
+  def save(self, redis_interface, key):
+    print(self.year + " - " + self.info)
+    redis_interface.save(key, self.year+" - "+self.info)
