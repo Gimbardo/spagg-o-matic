ab8362decfb6a34fdd50635e76a1f6937ce83bd3
Gamberi Elia <e.gamberi@engim.eu>
2023-07-08 20:24:35 +0200

    :bug: now syncs new facts on server start

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 import uvicorn
 from src.api import app
+from src.syncher import Syncher
 import logging
 
 if __name__ == "__main__":
+    syncher = Syncher()
+    syncher.synch()
     logging.getLogger().setLevel(logging.INFO)
     uvicorn.run(app, host='0.0.0.0', port=80, log_level="info")
