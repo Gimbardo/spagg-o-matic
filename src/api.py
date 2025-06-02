5f8521a2f5edd912833565240fe699f4062db54b
Elia <e.gamberi@advenias.it>
2025-06-02 15:33:58 +0200

    cors :)

--- src/api.py
+++ src/api.py
@@ -1,9 +1,25 @@
 from typing import Union
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.redis_interface import RedisInterface
 from constants import *
 
+origins = [
+    "https://gimbaro.dev",
+    "http://localhost",
+    "http://localhost:63342"
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 f = RedisInterface(host=REDIS_ADDRESS)
 
 
